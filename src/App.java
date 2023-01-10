b4e1790d6d00543e3c6e2e7c68031b7927726f22
Crystopher Carpenter <crystophercarpenter@pop-os.localdomain>
2023-01-10 14:24:57 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Digite o tamanho da pirâmide: ");
+
+        int dim = Integer.parseInt(System.console().readLine());
+
+        for (int i = 1; i <= dim; i++) {
+            String num = Integer.toString(i);
+
+            for (int j = 1; j < i; j++) {
+                num = num + i; 
+            }
+            System.out.println(num);
+        }
+    }
+}
